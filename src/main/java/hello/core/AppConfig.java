104cfce1eee34ca490af1940c955fd4c76a20ed1
DESKTOP-A0GNS6A\jinah <jinah0206@naver.com>
2023-01-28 20:04:34 +0900

    1-28 note-book

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -7,29 +7,31 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 public class AppConfig {
 
     //memberService 는 memberRepository 를 사용 - memberRepository는 MemoryMemberRepository를 사용
-//    @Bean
+    @Bean
     public MemberService memberService(){
         System.out.println("AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
-//    @Bean
+    @Bean
     public MemoryMemberRepository memberRepository() {
         System.out.println("AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     //orderService는 memberRepository. discountPolicy를 사용 - discountPolicy는 fixDiscountPolicy를 사용
-//    @Bean
+    @Bean
     public OrderService orderService(){
         System.out.println("AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
-//    @Bean
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
 //        return new FixDiscountPoliCy();
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,29 +3,27 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-//@RequiredArgsConstructor // 롬복 라이브러리 적용 -> final이 붙은 필드값을 생성자로 !!
+@RequiredArgsConstructor // 롬복 라이브러리 적용 -> final이 붙은 필드값을 생성자로 !!
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+    private final DiscountPolicy rateDiscountPolicy;
 
 
    
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = rateDiscountPolicy;
-    }
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.rateDiscountPolicy = rateDiscountPolicy;
+//    }
     @Override
     public  Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
-        int discountPrice = discountPolicy.discount(member,itemPrice);
+        int discountPrice = rateDiscountPolicy.discount(member,itemPrice);
 
         return new Order(memberId,itemName,itemPrice,discountPrice);
     }
